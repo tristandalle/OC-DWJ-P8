b0aafc838fa58a3fbceb56b7c516e5c5c79bee02
tristandalle <tristandalle@gmail.com>
2018-06-01 12:36:22 +0200

    template

---
+++ template.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <title><?= $title ?></title>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <link href="public/css/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+        <?= $content ?>
+    </body>
+    
+</html>
